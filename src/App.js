c14209aa26e5ebed19bc823b940a4534ca5aab8d
Monir <monirulslm7@gmail.com>
2021-09-18 23:46:24 +0600

    upload first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
+// counter component
+function Counter() {
+  let [count, setCount] = useState(0);
+  // console.log(count);
+  const increase = () => setCount(count + 1);
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  };
+  const setZero = () => setCount(count = 0);
+  return (
+    <div style={CounterStyle}>
+      <h2>Counter: {count}</h2>
+      <button style={ButtonStyle} onClick={increase}>Increase</button>
+      <button style={ButtonStyle} onClick={decrease}>Decrease</button>
+      <button style={ButtonStyle} onClick={setZero}>Reset</button>
+    </div>
+  )
+}
+// style declaration in object for counter.
+const CounterStyle = {
+  backgroundColor: 'skyblue',
+  border: '2px solid black',
+  padding: '10px'
+}
 
+const ButtonStyle = {
+  marginRight: '5px',
+  backgroundColor: '#4CAF50', /* Green */
+  border: 'none',
+  color: 'white',
+  padding: '15px 32px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '16px',
+  cursor: 'pointer'
+}
 export default App;
